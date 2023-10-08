d8f203484f6d6dee14fa8f29494f1c7ca09faf86
teoke <teoke246@gmail.com>
2023-10-08 17:04:50 +0700

    update githubaction for mobile test

--- src/main/java/com/teoke/provider/mobile/MyDemoAppAndroidProvider.java
+++ src/main/java/com/teoke/provider/mobile/MyDemoAppAndroidProvider.java
@@ -19,15 +19,15 @@ public class MyDemoAppAndroidProvider implements WebDriverProvider {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("teoke2");
         options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
-        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setPlatformName("android");
+//        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+//        options.setPlatformName("android");
 //        options.setPlatformName("Android");
 //        options.setPlatformVersion("7.1.1");
-//        options.setUdid("ce061716b8c2b0390d7e"); //ce061716b8c2b0390d7e // R5CT22QF5DD
+        options.setUdid("R5CT22QF5DD"); //ce061716b8c2b0390d7e // R5CT22QF5DD
 //        options.doesNoReset(MyDemoApp);
 //        options.setFullReset(false);
         try {
-            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),options);
+            return new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/com/teoke/tests/mobile/ApiDemosTest.java
+++ src/test/java/com/teoke/tests/mobile/ApiDemosTest.java
@@ -7,7 +7,7 @@ import com.teoke.tags.testtype.MobileTest;
 
 public class ApiDemosTest extends MobileTestSetup {
 
-    @MobileTest
+//    @MobileTest
     void scrollToElement(){
         HomeScreen homeScreen = ScreenObject.screen(HomeScreen.class);
 
